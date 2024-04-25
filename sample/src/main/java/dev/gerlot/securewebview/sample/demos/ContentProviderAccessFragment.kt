53db449c613d2a15961f8a81d4ce3eeb5f9e01af
Balázs Gerlei <gerlotdev@gmail.com>
2024-04-25 13:31:41 +0200

    7 incorrect color for the clear url field button in the sample app in dark mode (#10)
    * Fix #7: Apply the primary text color to the clear button displayed at the end of the URL input field in the sample app
    
    * Add missing checks for whether View of the Fragments are still attached before updating UI in onPageStarted in the sample app

--- sample/src/main/java/dev/gerlot/securewebview/sample/demos/ContentProviderAccessFragment.kt
+++ sample/src/main/java/dev/gerlot/securewebview/sample/demos/ContentProviderAccessFragment.kt
@@ -69,6 +69,7 @@ class ContentProviderAccessFragment: Fragment(), SecurableWebViewFragment {
 
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 currentUrl = url
+                view ?: return
                 binding.urlInput.setText(url)
             }
 
